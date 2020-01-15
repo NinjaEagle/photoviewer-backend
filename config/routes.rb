db66f9746c7985d776820c6cf1e67f20f3dff02e
Kevin Wang <kcwang06@gmail.com>
2020-01-15 11:40:01 -0500

    heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :images 
+
+  # set up another route to get all the pictures that isn't limiting 10 images 
   get '/get_All_Pictures', to: 'images#get_All_Pictures'
 end
